d3f080243804ce3d3823488cb3ab77f19d8e53df
Evenstar Nikole <enikole@il-a1.21-school.ru>
2019-04-07 20:20:00 +0300

    2

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,26 +6,16 @@
 /*   By: enikole <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/07 12:29:28 by enikole           #+#    #+#             */
-/*   Updated: 2019/04/07 12:39:44 by enikole          ###   ########.fr       */
+/*   Updated: 2019/04/07 20:16:50 by enikole          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include "libft.h"
 
 void		ft_memdel(void **ap)
 {
-	size_t	len;
-	size_t	i;
-
-	i = 0;
 	if (!ap)
 		return ;
-	len = ft_strlen(*ap);
-	while (i < len)
-	{
-		free(ap[i]);
-		i++;
-	}
+	free(*ap);
 	*ap = NULL;
 }
